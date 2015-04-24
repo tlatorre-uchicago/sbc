f18fa2fbc614b5f48a5a8570bcf14823b1e1c5e2
Anthony LaTorre <tlatorre@uchicago.edu>
2015-04-24 09:44:55 -0500

    kqueue works differently than epoll. most importantly, events are not a bitmask, so a read event and write event are registered separately.

--- src/epoll.c
+++ src/epoll.c
@@ -14,18 +14,27 @@ int epoll_create(int size)
 int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event)
 {
     static struct kevent evSet;
+    static int rv;
 
-    short filter = 0;
-    if (event->events & EPOLLIN)  filter |= EVFILT_READ;
-    if (event->events & EPOLLOUT) filter |= EVFILT_WRITE;
-
-    unsigned short flags = 0;
-    if (op == EPOLL_CTL_ADD) flags = EV_ADD;
-    if (op == EPOLL_CTL_MOD) flags = EV_ADD;
-    if (op == EPOLL_CTL_DEL) flags = EV_DELETE;
-
-    EV_SET(&evSet, fd, filter, flags, 0, 0, (void *)event->data.ptr);
-    return kevent(epfd, &evSet, 1, NULL, 0, NULL);
+    if (op == EPOL_CTL_ADD || op == EPOLL_CTL_MOD) {
+        if (event->events & EPOLLIN) {
+            EV_SET(&evSet, fd, EVFILT_READ, EV_ADD, 0, 0, event->data.ptr);
+            if ((rv = kevent(epfd, &evSest, 1, NULL, 0, NULL))) return rv;
+            }
+        if (event->events & EPOLLOUT) {
+            EV_SET(&evSet, fd, EVFILT_WRITE, EV_ADD, 0, 0, event->data.ptr);
+            if ((rv = kevent(epfd, &evSet, 1, NULL, 0, NULL))) return rv;
+        }
+    } else if (op == EPOLL_CTL_DEL) {
+        EV_SET(&evSet, fd, EVFILT_READ, EV_DELETE, 0, 0, 0);
+        rv = kevent(epfd, &evSet, 1, NULL, 0, NULL);
+        if (rv != ENOENT && rv != 0) return rv;
+        EV_SET(&evSet, fd, EVFILT_WRITE, EV_DELETE, 0, 0, 0);
+        rv = kevent(epfd, &evSet, 1, NULL, 0, NULL);
+        if (rv == ENOENT) return 0;
+        return rv;
+    }
+    return 0;
 }
 
 int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
@@ -33,8 +42,8 @@ int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
     static struct kevent evList[MAX_EVENTS];
     static struct timespec _timeout;
 
-    _timeout.tv_nsec = 0;
-    _timeout.tv_sec = timeout;
+    _timeout.tv_nsec = timeout*1000000;
+    _timeout.tv_sec = 0;
 
     int nev, i;
 
@@ -47,11 +56,13 @@ int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
 
     for (i = 0; i < nev; i++) {
         events[i].events = 0;
-        if (evList[i].flags & EVFILT_READ)  events[i].events |= EPOLLIN;
-        if (evList[i].flags & EVFILT_WRITE) events[i].events |= EPOLLOUT;
-        if (evList[i].flags & EV_ERROR)     events[i].events |= EPOLLERR;
-        /* EPOLLHUP seems like the closest thing to EV_EOF */
-        if (evList[i].flags & EV_EOF)       events[i].events |= EPOLLHUP;
+        if (evList[i].filter == EVFILT_READ) {
+            events[i].events |= EPOLLIN;
+        } else if (evList[i].filter == EVFILT_WRITE) {
+            events[i].events |= EPOLLOUT;
+        }
+        if (evList[i].flags & EV_ERROR) events[i].events |= EPOLLERR;
+        if (evList[i].flags & EV_EOF)   events[i].events |= EPOLLHUP;
 
         events[i].data.ptr = (void *)evList[i].udata;
     }
